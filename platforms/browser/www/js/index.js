4a71b073a814997059e0f361c5f6f0b3ab2aea37
Nour._07 <nour.sendyan.10@gmail.com>
2023-02-17 18:51:13 +0100

    1.9

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,3 +1,4 @@
+// Importar funciones
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import {
   getAuth,
@@ -32,79 +33,94 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getDatabase(app);
-let user = null;
-let clasificacionEquipos = null;
+
+// INICIO DE SESIÓN CON GOOGLE
+// Declaración de las variables "user" y "clasificacionEquipos"
+let user,
+  clasificacionEquipos = null;
+// Agregar un evento "deviceready" al documento para llamar a la función "onDeviceReady" cuando el dispositivo esté listo.
 document.addEventListener("deviceready", onDeviceReady, false);
-// con google
+// Función que se ejecutará cuando el dispositivo esté listo.
 function onDeviceReady() {
+  // Llamada a la función "getRed".
   getRed();
+  // Agregar un evento "click" botón de Google para iniciar sesión con Google.
   document.querySelector("#googleLogin").addEventListener("click", function () {
+    // Crear una instancia del proveedor de autenticación de Google.
     const provider = new GoogleAuthProvider(auth);
+    // Iniciar sesión con redireccionamiento y, cuando se complete, llamar a la función "getRed" para actualizar la página.
     signInWithRedirect(auth, provider).then(() => {
       getRed();
     });
   });
+  // Definición de la función "getRed".
   function getRed() {
+    // Obtener el resultado de la redirección de inicio de sesión de Google.
     getRedirectResult(auth)
       .then((result) => {
-        // const credential = GoogleAuthProvider.credentialFromResult(result);
-        // const token = credential.accessToken;
+        // Extraer la información del usuario del resultado y asignarla a la variable "user_info".
         const user_info = result.user;
-        console.log("User info: ", user_info);
+        // console.log("User info: ", user_info);
+        // Extraer el nombre de usuario a partir de la dirección de correo electrónico y asignarlo a la variable "user".
         user = user_info.email
           .slice(0, user_info.email.indexOf("@"))
           .replaceAll(".", " ");
+        // Extraer el nombre del usuario de la información del usuario y asignarlo a la variable "name".
         let name = user_info.displayName;
-
+        // Se ocultan las opciones de inicio de sesión y se muestra la opción de logout.
         const pageLogin = document.querySelectorAll("#page-login li");
         pageLogin[0].classList.add("d-none");
         pageLogin[1].classList.add("d-none");
         pageLogin[2].classList.remove("d-none");
-
+        // Oculta la sección de bienvenida y muestra la sección principal
         const pageWelcome = document.getElementById("page-main-info");
         pageWelcome.classList.remove("d-block");
         pageWelcome.classList.add("d-none");
-
         const pageMain = document.getElementById("page-main");
         pageMain.classList.remove("d-none");
         pageMain.classList.add("d-block");
-
         const footer = document.getElementById("footer");
         footer.classList.remove("fixed-bottom");
-
+        // Mostrar un mensaje de bienvenida en la página principal con el nombre del usuario.
         document.getElementById("page-main-welcome").innerHTML =
-          "Bienvenido " + name + "<br>";
+          "Bienvenido " + name;
+        // Obtener una referencia a la base de datos para el usuario actual.
         let root_ref = ref(db, "users/" + user);
+        // Establecer los datos del usuario en la base de datos.
         set(root_ref, {
           nombre: name,
           usuario: user,
           email: user_info.email,
         });
-        clasificacionEquipos = ref(db, "/Equipos/");
-        getClasificacionEquipos(clasificacionEquipos);
+        // Llamada a la función "getClasificacionEquipos".
+        getClasificacionEquipos();
       })
       .catch((error) => {
         console.log(error);
       });
   }
 }
-// con github
+
+// INICIO DE SESIÓN CON GITHUB
+// Se selecciona el botón de Github
 const githubButton = document.querySelector("#githubLogin");
+// Se añade un evento de clic al botón de Github
 githubButton.addEventListener("click", (e) => {
-  e.preventDefault();
+  // Se crea una instancia del proveedor de autenticación de Github
   const provider = new GithubAuthProvider();
+  // Se llama a la función signInWithPopup() para autenticar al usuario con Github
   signInWithPopup(auth, provider)
     .then((credentials) => {
-      console.log(credentials);
-
-      // Close the login modal
+      // console.log(credentials);
+      // Se cierra el modal de inicio de sesión
       const modal = bootstrap.Modal.getInstance(signInForm.closest(".modal"));
       modal.hide();
-
+      // Se ocultan las opciones de inicio de sesión y se muestra la opción de logout.
       const pageLogin = document.querySelectorAll("#page-login li");
       pageLogin[0].classList.add("d-none");
       pageLogin[1].classList.add("d-none");
       pageLogin[2].classList.remove("d-none");
+      // Oculta la sección de bienvenida y muestra la sección principal
       const pageWelcome = document.getElementById("page-main-info");
       pageWelcome.classList.remove("d-block");
       pageWelcome.classList.add("d-none");
@@ -114,40 +130,44 @@ githubButton.addEventListener("click", (e) => {
       const footer = document.getElementById("footer");
       footer.classList.remove("fixed-bottom");
       let user = credentials.user.displayName;
+      // Mostrar un mensaje de bienvenida en la página principal con el nombre del usuario.
       document.getElementById("page-main-welcome").innerHTML =
-        "Bienvenido " + user + "<br>";
-
-      const clasificacionEquipos = ref(db, "/Equipos/");
-      getClasificacionEquipos(clasificacionEquipos);
+        "Bienvenido " + user;
+      // Llamada a la función "getClasificacionEquipos".
+      getClasificacionEquipos();
     })
     .catch((error) => {
+      // Se muestra un modal de error en caso de que ocurra un error durante la autenticación
       lanzarModal(error);
     });
 });
-
-// Registrarse con correo y contraseña
+// REGISTRARSE CON CORREO Y CONTRASEÑA
+// Selecciona el formulario de registro en el HTML
 const signUpForm = document.querySelector("#signup-form");
+// Añade un evento de "submit" al formulario de registro
 signUpForm.addEventListener("submit", (e) => {
+  // Previene que se envíe el formulario y se recargue la página.
   e.preventDefault();
+  // Obtiene el valor del campo de correo electrónico y de contraseña del formulario.
   const email = signUpForm["signup-email"].value;
   const password = signUpForm["signup-password"].value;
-
+  // Crea un usuario con la dirección de correo electrónico y la contraseña proporcionadas.
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      console.log(userCredential);
-
-      // Close the signup modal
+      // Imprime en la consola el objeto del usuario creado.
+      // console.log(userCredential);
+      // Cierra la ventana modal de registro.
       const signupModal = document.querySelector("#signupModal");
       const modal = bootstrap.Modal.getInstance(signupModal);
       modal.hide();
-
-      // reset the form
+      // Reinicia el formulario
       signUpForm.reset();
-
+      // Se ocultan las opciones de inicio de sesión y se muestra la opción de logout.
       const pageLogin = document.querySelectorAll("#page-login li");
       pageLogin[0].classList.add("d-none");
       pageLogin[1].classList.add("d-none");
       pageLogin[2].classList.remove("d-none");
+      // Oculta la sección de bienvenida y muestra la sección principal
       const pageWelcome = document.getElementById("page-main-info");
       pageWelcome.classList.remove("d-block");
       pageWelcome.classList.add("d-none");
@@ -156,16 +176,18 @@ signUpForm.addEventListener("submit", (e) => {
       pageMain.classList.add("d-block");
       const footer = document.getElementById("footer");
       footer.classList.remove("fixed-bottom");
+      // Crea una variable "user" a partir de la dirección de correo electrónico del usuario creado para guradar el nombre de usuario.
       let user = userCredential.user.email
         .slice(0, userCredential.user.email.indexOf("@"))
         .replaceAll(".", " ");
+      // Mostrar un mensaje de bienvenida en la página principal con el nombre del usuario.
       document.getElementById("page-main-welcome").innerHTML =
-        "Bienvenido " + user + "<br>";
-
-      const clasificacionEquipos = ref(db, "/Equipos/");
-      getClasificacionEquipos(clasificacionEquipos);
+        "Bienvenido " + user;
+      // Llamada a la funcion "getClasificacionEquipos".
+      getClasificacionEquipos();
     })
     .catch((error) => {
+      // Muestra un model con el mensaje de error si se produce algún error durante la creación del usuario.
       if (error.code === "auth/email-already-in-use") {
         lanzarModal("El correo ya está en uso");
       } else if (error.code === "auth/invalid-email") {
@@ -177,27 +199,32 @@ signUpForm.addEventListener("submit", (e) => {
       }
     });
 });
-
-// Iniciar sesion con correo y contraseña
+// INICIO DE SESIÓN CON CORREO Y CONTRASEÑA
+// Selecciona el formulario de inicio de sesión en el HTML.
 const signInForm = document.querySelector("#login-form");
+// Agrega un event listener al formulario de inicio de sesión que se activa cuando se hace clic en "submit".
 signInForm.addEventListener("submit", (e) => {
+  // Previene que se envíe el formulario y se recargue la página.
   e.preventDefault();
+  // Obtiene el valor del campo de correo electrónico y de contraseña del formulario.
   const email = signInForm["login-email"].value;
   const password = signInForm["login-password"].value;
-
+  // Inicia sesión con el correo electrónico y la contraseña proporcionados usando el servicio de autenticación de Firebase y devuelve un objeto userCredential.
   signInWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-      console.log(userCredential);
-      // Close the login modal
+      // Imprime el objeto userCredential en la consola
+      // console.log(userCredential);
+      // Cierra el modal de inicio de sesión
       const modal = bootstrap.Modal.getInstance(signInForm.closest(".modal"));
       modal.hide();
-      // reset the form
+      // Restablece el formulario de inicio de sesión
       signInForm.reset();
-
+      // Se ocultan las opciones de inicio de sesión y se muestra la opción de logout.
       const pageLogin = document.querySelectorAll("#page-login li");
       pageLogin[0].classList.add("d-none");
       pageLogin[1].classList.add("d-none");
       pageLogin[2].classList.remove("d-none");
+      // Oculta la sección de bienvenida y muestra la sección principal
       const pageWelcome = document.getElementById("page-main-info");
       pageWelcome.classList.remove("d-block");
       pageWelcome.classList.add("d-none");
@@ -206,33 +233,46 @@ signInForm.addEventListener("submit", (e) => {
       pageMain.classList.add("d-block");
       const footer = document.getElementById("footer");
       footer.classList.remove("fixed-bottom");
+      // Crea una variable "user" a partir de la dirección de correo electrónico del usuario creado para guradar el nombre de usuario.
       let user = userCredential.user.email
         .slice(0, userCredential.user.email.indexOf("@"))
         .replaceAll(".", " ");
+      // Mostrar un mensaje de bienvenida en la página principal con el nombre del usuario.
       document.getElementById("page-main-welcome").innerHTML =
         "Bienvenido " + user + "<br>";
-
-      const clasificacionEquipos = ref(db, "/Equipos/");
-      getClasificacionEquipos(clasificacionEquipos);
+      // Llamada a la funcion "getClasificacionEquipos".
+      getClasificacionEquipos();
     })
     .catch((error) => {
+      // Si el error es debido a una contraseña incorrecta, lanza un modal con el mensaje "Contraseña incorrecta"
       if (error.code === "auth/wrong-password") {
         lanzarModal("Contraseña incorrecta");
-      } else if (error.code === "auth/user-not-found") {
+      }
+      // Si el error es debido a que el correo electrónico no está registrado, lanza un modal con el mensaje "Correo no encontrado"
+      else if (error.code === "auth/user-not-found") {
         lanzarModal("Correo no encontrado");
-      } else {
+      }
+      // En cualquier otro caso, lanza un modal con el mensaje "Otro error!!!"
+      else {
         lanzarModal("Otro error!!!");
       }
     });
 });
-
-// Imprimir la tabla desde la base de datos realtime
-function getClasificacionEquipos(clasificacionEquipos) {
+// IMPRIMIR LOS EQUPOS DESDE LA BASE DE DATOS
+// La función getClasificacionEquipos se encarga de obtener los datos de clasificación de equipos desde la base de datos.
+function getClasificacionEquipos() {
+  // Obtiene una referencia a la clasificación de equipos en la base de datos.
+  clasificacionEquipos = ref(db, "/Equipos/");
+  // Si la referencia a la clasificación de equipos no es nula, se procede a añadir un escucha de eventos para detectar cuando se añaden nuevos datos.
   if (clasificacionEquipos != null) {
     onChildAdded(clasificacionEquipos, (data) => {
+      // Se obtiene el valor de los datos que se han añadido.
       data = data.val();
+      // Se obtiene una referencia a la tabla donde se van a añadir los datos.
       const table = document.querySelector("#myTable");
+      // Se obtiene una referencia al cuerpo de la tabla donde se van a añadir los datos.
       const tableBody = table.querySelector("tbody");
+      // Se construye una nueva fila con los datos que se han obtenido.
       const newRow = `<tr>
                         <td><b>${data.Posicion}</b></td>
                         <td>${data.Nombre_del_equipo}</td>
@@ -242,7 +282,9 @@ function getClasificacionEquipos(clasificacionEquipos) {
                         <td>${data.Partidos_empatados}</td>
                         <td>${data.Partidos_perdidos}</td>
                       </tr>`;
+      // Se añade la nueva fila al cuerpo de la tabla.
       tableBody.innerHTML += newRow;
+      // Llamada a la función "pintarCelda".
       pintarCelda();
     });
   }
@@ -292,6 +334,7 @@ function pintarCelda() {
   }
 }
 
+// MOSTRAR UN MODAL
 function lanzarModal(texto) {
   // Crear el elemento del modal
   let modal = document.createElement("div");
@@ -324,7 +367,8 @@ function lanzarModal(texto) {
   // Mostrar el modal
   $(modal).modal("show");
 }
-// Logout
+
+// LOGOUT
 const logout = document.querySelector("#logout");
 logout.addEventListener("click", async (e) => {
   e.preventDefault();
